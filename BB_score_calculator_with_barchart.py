d7edd74ef08711f2f2fefcef32b20bb073fb65f9
Phil Simon <phil@philsimon.com>
2019-04-21 08:37:55 -0700

    Added chart title
    I should have done this the first time.

--- BB_score_calculator_with_barchart.py
+++ BB_score_calculator_with_barchart.py
@@ -3,8 +3,11 @@ import time
 gp = int(input("How many games did you play? "))
 tot_shots = 0
 tot_makes = 0
+game_list = [] # create empty list for x-axis
+pct_list = [] # create empty list for y-axis
 for j in range(gp):
     print("Let's talk about game",j+1,".")
+    game_list.append(j+1) # append to game_list
     if int(j) < gp:
         counter = 0
         no_shots = int(input('How many shots did you take in the game? '))
@@ -26,6 +29,7 @@ for j in range(gp):
         print("****", "\t**", "\t***", " \t***")
         if no_shots > 0:
             pct = round(100*(counter/no_shots),3)
+            pct_list.append(pct) # append to pct_list
             print(j+1, "\t",counter, "\t",no_shots,"\t",pct)
         else:
             print(j+1, "\t",counter, "\t",no_shots,"\t",0)    
@@ -47,6 +51,23 @@ print('Thinking...')
 print()
 time.sleep(2)
 print("Games", "\tFG", "\tFGA", "  \tPCT")
-print("*****", "\t**", "\t***", "  \t***")
+print("****", "\t**", "\t***", "  \t***")
 print("  ",gp, "\t",tot_makes, "\t",tot_shots,"\t",tot_pct)
 print()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Let's see your shot distribution graphically.")
+
+y_pos = np.arange(len(game_list))
+plt.title("Phil's shooting percentage by game")
+ 
+# Graph percentages on y-axis
+plt.bar(y_pos, pct_list)
+ 
+# Graph games on x-axis
+plt.xticks(y_pos, game_list)
+ 
+# Show graphic
+plt.show()
